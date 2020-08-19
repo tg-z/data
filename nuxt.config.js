5bebebb0c9f245c40d4d4bfd6b56b86635893f79
serafina <parasitism@protonmail.com>
2020-08-19 15:46:47 -0700

    default remark plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,31 @@
-{
-  modules: [
-    '@nuxt/content'
-  ],
+export default {
   content: {
-    dir: 'content',
     apiPrefix: 'content',
-    fullTextSearchFields: [ 'title', 'tags', 'description', 'text', 'slug'],
+    dir: 'content',
+    fullTextSearchFields: ['title', 'description', 'slug', 'text', 'tags'],
+    nestedProperties: [],
     liveEdit: true,
-    markdown:
+    markdown: {
+      remarkPlugins: [
+        'remark-squeeze-paragraphs',
+        'remark-slug',
+        'remark-autolink-headings',
+        'remark-external-links',
+        'remark-footnotes'
+      ],
+      rehypePlugins: [
+        'rehype-minify-whitespace',
+        'rehype-sort-attribute-values',
+        'rehype-sort-attributes',
+        'rehype-raw'
+      ],
+      prism: {
+        theme: 'prismjs/themes/prism.css'
+      }
+    },
+    yaml: {},
+    csv: {},
+    xml: {},
+    extendParser: {}
   }
 }
